8f0ede6e7ffd0d241cf97acfda5d22829a896ef6
neimers <40810085+neimers@users.noreply.github.com>
2020-11-20 14:51:48 -0800

    add batch rename files script

---
+++ file-manipulation/bulk_rename_files_in_folder.ps1
@@ -0,0 +1,10 @@
+﻿# ============= SET THESE VALUES =================
+$find = "test" # will wildcard search this text
+$replace = "replaced" # will replace search text with this text
+$search_directory = "D:\temp" # this is the directory that will be searched (non recursive)
+# ================================================
+
+cd $search_directory
+$find_text = "*" + $find + "*"
+
+Get-ChildItem -Filter $find_text | Rename-Item -NewName {$_.name -replace $find, $replace }

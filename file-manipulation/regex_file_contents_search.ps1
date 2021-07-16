5928fbaaa0bc8347ade69036e0ce4fe04bcb62a3
neimers <40810085+neimers@users.noreply.github.com>
2021-07-16 11:22:38 -0700

    added example for searching files with regex

---
+++ file-manipulation/regex_file_contents_search.ps1
@@ -0,0 +1,34 @@
+
+# Get Directory This Script is In (Assume Will Be Root For Everything Else)
+$invocation = (Get-Variable MyInvocation).Value
+$directorypath = Split-Path $invocation.MyCommand.Path
+#$settingspath = $directorypath + '\settings.xml'
+
+# Output File to Write Matches Into
+$ResultsCSV = "C:\TEMP\output\Results.csv" 
+
+# Directory to Search Within
+$Directory = $directorypath + "\demo-files"
+
+# Regex Expression to Match Against
+$RX = "(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}"
+
+$TextFiles = Get-ChildItem $Directory -Include *.txt*,*.csv*,*.rtf*,*.eml*,*.msg*,*.dat*,*.ini*,*.mht* -Recurse
+
+ $file2 =  new-object System.IO.StreamWriter($ResultsCSV) #output Stream
+ $file2.WriteLine('Matches,File Path') # write header
+
+foreach ($FileSearched in $TextFiles) {   #loop over files in folder
+
+    #    $text = [IO.File]::ReadAllText($FileSearched)
+    $file = New-Object System.IO.StreamReader ($FileSearched)  # Input Stream
+
+    while ($text = $file.ReadLine()) {      # read line by line
+        foreach ($match in ([regex]$RX).Matches($text)) {   
+               # write line to output stream
+               $file2.WriteLine("{0},{1}",$match.Value, $FileSearched.fullname )  
+        } #foreach $match
+    }#while $file
+     $file.close();  
+} #foreach  
+$file2.close()
